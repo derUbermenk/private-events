3135ddb5ca82d9f1aa28cf7aa8e86d24b6ec81b2
Chester Asuncion <asuncionmcv@gmail.com>
2021-12-26 18:07:15 +0800

    changing event builds, will use associations with user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit delete]
+
   def show
-    @events_created = User.events.all
+    @events_created = @user.events.all
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
