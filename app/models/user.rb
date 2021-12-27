55957de008e729cee413aae4a5184cfae9e932d7
Chester Asuncion <asuncionmcv@gmail.com>
2021-12-27 20:21:28 +0800

    adding a scope in user to get all except current

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :events, foreign_key: :creator_id
+  has_many :events, foreign_key: :creator_id, dependent: :destroy
 
-  has_many :attendances, foreign_key: :attendee_id
-  has_many :attended_events, through: :attendances, source: :event
+  has_many :attendances, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attended_events, through: :attendances, source: :event, dependent: :destroy
+
+  has_many :invitations_received, foreign_key: :invitee_id, dependent: :destroy, class_name: 'Invitation'
+  has_many :invited_events, through: :invitations, source: :event
+  has_many :invitations_sent, foreign_key: :organizer_id, dependent: :destroy, class_name: 'Invitation'
+
+  scope :all_except, ->(user) { where.not(id: user) }
 end
